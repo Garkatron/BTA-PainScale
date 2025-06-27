4dfdab58fda3c0ebffe9994bacbd93f2ebc20588
Garkatron <145294535+Garkatron@users.noreply.github.com>
2025-06-28 01:29:31 +0200

    Added health multiplier

---
+++ src/main/java/deus/painscale/mixin/MobMixin.java
@@ -0,0 +1,15 @@
+package deus.painscale.mixin;
+
+import net.minecraft.core.entity.Entity;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+@Mixin(value = net.minecraft.core.entity.Mob.class)
+public abstract class MobMixin {
+
+
+}
